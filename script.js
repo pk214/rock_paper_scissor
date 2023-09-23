b1cd4bf3fa4808f1a7968168fcc9ad8a7e6c6195
pk214 <piyushkrs214@gmail.com>
2023-09-23 22:24:14 +0530

    Adding new feture of Leaderboard

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function play(userChoice) {
   const result = getResult(userChoice, computerChoice);
 
   displayResult(userChoice, computerChoice, result);
+  displayLeader(userChoice, computerChoice, result);
 }
 
 function getResult(user, computer) {
@@ -17,26 +18,49 @@ function getResult(user, computer) {
     (user === "scissors" && computer === "paper")
   ) {
     a = a + 1;
-    if(c>0){
-      c=c-1;
+    if (c > 0) {
+      c = c - 1;
     }
-    else{
-      c=0;
+    else {
+      c = 0;
     }
     return "You Win! 🔥";
   }
   c = c + 1;
-  if(a>0){
-    a=a-1;
+  if (a > 0) {
+    a = a - 1;
   }
-  else{
-    a=0;
+  else {
+    a = 0;
   }
   return "Opponent Win! 😢";
 }
 
 function displayResult(user, computer, result) {
   const resultElement = document.getElementById("result");
-  
+
   resultElement.innerHTML = `You choose ${user}.<br>Opponent choose ${computer}.<br>${result}<br>Your Score: ${a}<br>Opponent Score: ${c}`;
+}
+
+function displayLeader(user, computer, result) {
+  const leaderboardElement = document.getElementById("win_time");
+
+  leaderboardElement.innerHTML = `${result}`;
+}
+
+function getEmoji(a, c) {
+  if (a > c) {
+    return `Player ${a} 👑`;
+  }
+  else {
+    return `Computer ${c} 👑`;
+  }
+}
+
+const winner_tag = getEmoji(a, c);
+
+function displayEmoji(winner_tag) {
+  const EmojiElement = document.getElementById("win_icon");
+
+  EmojiElement.innerHTML = `${winner_tag}`;
 }
